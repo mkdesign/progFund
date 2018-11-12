803f2a5e15ad36a278fc71d2c2f498b340b90a63
mkdesign <mehrad.kavian@gmail.com>
2018-11-12 15:28:02 +0000

    Solution Folder

---
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtCr1 = new System.Windows.Forms.TextBox();
+            this.lblCrTitle1 = new System.Windows.Forms.Label();
+            this.lblCrTitle2 = new System.Windows.Forms.Label();
+            this.txtCr2 = new System.Windows.Forms.TextBox();
+            this.lblCrTitle3 = new System.Windows.Forms.Label();
+            this.txtCr3 = new System.Windows.Forms.TextBox();
+            this.lblCrTitle4 = new System.Windows.Forms.Label();
+            this.txtCr4 = new System.Windows.Forms.TextBox();
+            this.lblCrTitle5 = new System.Windows.Forms.Label();
+            this.txtCr5 = new System.Windows.Forms.TextBox();
+            this.lblClassAvg = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.lblClassTitle = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtCr6 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtCr1
+            // 
+            this.txtCr1.Location = new System.Drawing.Point(170, 62);
+            this.txtCr1.Name = "txtCr1";
+            this.txtCr1.Size = new System.Drawing.Size(100, 20);
+            this.txtCr1.TabIndex = 0;
+            // 
+            // lblCrTitle1
+            // 
+            this.lblCrTitle1.AutoSize = true;
+            this.lblCrTitle1.Location = new System.Drawing.Point(301, 69);
+            this.lblCrTitle1.Name = "lblCrTitle1";
+            this.lblCrTitle1.Size = new System.Drawing.Size(35, 13);
+            this.lblCrTitle1.TabIndex = 1;
+            this.lblCrTitle1.Text = "label1";
+            // 
+            // lblCrTitle2
+            // 
+            this.lblCrTitle2.AutoSize = true;
+            this.lblCrTitle2.Location = new System.Drawing.Point(301, 105);
+            this.lblCrTitle2.Name = "lblCrTitle2";
+            this.lblCrTitle2.Size = new System.Drawing.Size(35, 13);
+            this.lblCrTitle2.TabIndex = 3;
+            this.lblCrTitle2.Text = "label2";
+            // 
+            // txtCr2
+            // 
+            this.txtCr2.Location = new System.Drawing.Point(170, 102);
+            this.txtCr2.Name = "txtCr2";
+            this.txtCr2.Size = new System.Drawing.Size(100, 20);
+            this.txtCr2.TabIndex = 2;
+            // 
+            // lblCrTitle3
+            // 
+            this.lblCrTitle3.AutoSize = true;
+            this.lblCrTitle3.Location = new System.Drawing.Point(301, 144);
+            this.lblCrTitle3.Name = "lblCrTitle3";
+            this.lblCrTitle3.Size = new System.Drawing.Size(35, 13);
+            this.lblCrTitle3.TabIndex = 5;
+            this.lblCrTitle3.Text = "label3";
+            // 
+            // txtCr3
+            // 
+            this.txtCr3.Location = new System.Drawing.Point(170, 141);
+            this.txtCr3.Name = "txtCr3";
+            this.txtCr3.Size = new System.Drawing.Size(100, 20);
+            this.txtCr3.TabIndex = 4;
+            // 
+            // lblCrTitle4
+            // 
+            this.lblCrTitle4.AutoSize = true;
+            this.lblCrTitle4.Location = new System.Drawing.Point(301, 185);
+            this.lblCrTitle4.Name = "lblCrTitle4";
+            this.lblCrTitle4.Size = new System.Drawing.Size(35, 13);
+            this.lblCrTitle4.TabIndex = 7;
+            this.lblCrTitle4.Text = "label4";
+            // 
+            // txtCr4
+            // 
+            this.txtCr4.Location = new System.Drawing.Point(170, 182);
+            this.txtCr4.Name = "txtCr4";
+            this.txtCr4.Size = new System.Drawing.Size(100, 20);
+            this.txtCr4.TabIndex = 6;
+            // 
+            // lblCrTitle5
+            // 
+            this.lblCrTitle5.AutoSize = true;
+            this.lblCrTitle5.Location = new System.Drawing.Point(301, 228);
+            this.lblCrTitle5.Name = "lblCrTitle5";
+            this.lblCrTitle5.Size = new System.Drawing.Size(35, 13);
+            this.lblCrTitle5.TabIndex = 9;
+            this.lblCrTitle5.Text = "label5";
+            // 
+            // txtCr5
+            // 
+            this.txtCr5.Location = new System.Drawing.Point(170, 225);
+            this.txtCr5.Name = "txtCr5";
+            this.txtCr5.Size = new System.Drawing.Size(100, 20);
+            this.txtCr5.TabIndex = 8;
+            // 
+            // lblClassAvg
+            // 
+            this.lblClassAvg.AutoSize = true;
+            this.lblClassAvg.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F);
+            this.lblClassAvg.Location = new System.Drawing.Point(555, 90);
+            this.lblClassAvg.Name = "lblClassAvg";
+            this.lblClassAvg.Size = new System.Drawing.Size(81, 29);
+            this.lblClassAvg.TabIndex = 10;
+            this.lblClassAvg.Text = "label6";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(557, 62);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(75, 13);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Class Average";
+            // 
+            // lblClassTitle
+            // 
+            this.lblClassTitle.AutoSize = true;
+            this.lblClassTitle.Location = new System.Drawing.Point(560, 144);
+            this.lblClassTitle.Name = "lblClassTitle";
+            this.lblClassTitle.Size = new System.Drawing.Size(35, 13);
+            this.lblClassTitle.TabIndex = 12;
+            this.lblClassTitle.Text = "label8";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(170, 282);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(302, 259);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "label5";
+            // 
+            // txtCr6
+            // 
+            this.txtCr6.Location = new System.Drawing.Point(170, 256);
+            this.txtCr6.Name = "txtCr6";
+            this.txtCr6.Size = new System.Drawing.Size(100, 20);
+            this.txtCr6.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtCr6);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblClassTitle);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.lblClassAvg);
+            this.Controls.Add(this.lblCrTitle5);
+            this.Controls.Add(this.txtCr5);
+            this.Controls.Add(this.lblCrTitle4);
+            this.Controls.Add(this.txtCr4);
+            this.Controls.Add(this.lblCrTitle3);
+            this.Controls.Add(this.txtCr3);
+            this.Controls.Add(this.lblCrTitle2);
+            this.Controls.Add(this.txtCr2);
+            this.Controls.Add(this.lblCrTitle1);
+            this.Controls.Add(this.txtCr1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtCr1;
+        private System.Windows.Forms.Label lblCrTitle1;
+        private System.Windows.Forms.Label lblCrTitle2;
+        private System.Windows.Forms.TextBox txtCr2;
+        private System.Windows.Forms.Label lblCrTitle3;
+        private System.Windows.Forms.TextBox txtCr3;
+        private System.Windows.Forms.Label lblCrTitle4;
+        private System.Windows.Forms.TextBox txtCr4;
+        private System.Windows.Forms.Label lblCrTitle5;
+        private System.Windows.Forms.TextBox txtCr5;
+        private System.Windows.Forms.Label lblClassAvg;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lblClassTitle;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtCr6;
+    }
+}
+
---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            decimal[] classArray = new decimal[6];
+            decimal average =0;
+            string[] classTitles = new string[6];
+            
+            classArray[0] = Convert.ToInt32(txtCr1.Text);
+            classArray[1] = Convert.ToInt32(txtCr2.Text);
+            classArray[2] = Convert.ToInt32(txtCr3.Text);
+            classArray[3] = Convert.ToInt32(txtCr4.Text);
+            classArray[4] = Convert.ToInt32(txtCr5.Text);
+            classArray[5] = Convert.ToInt32(txtCr6.Text);
+
+            for (var i =0; i < 6; i++)
+            {
+                if(classArray[i] <= 100 && classArray[i] >=80)
+                {
+                    classTitles[i] = "A";
+                }else if (classArray[i] < 80 && classArray[i] >= 70)
+                {
+                    classTitles[i] = "B";
+                }
+                else if (classArray[i] < 70 && classArray[i] >= 60)
+                {
+                    classTitles[i] = "C";
+                }
+                else if (classArray[i] < 60 && classArray[i] >= 50)
+                {
+                    classTitles[i] = "D";
+                }
+                else 
+                {
+                    classTitles[i] = "F";
+                }
+
+                average = ( average + classArray[i]);
+       
+                
+            }
+
+            lblClassAvg.Text = Convert.ToString(average/6);
+
+            if (average <= 100 && average >= 80)
+            {
+                lblClassTitle.Text = "A";
+            }
+            else if (average < 80 && average >= 70)
+            {
+                lblClassTitle.Text = "B";
+            }
+            else if (average < 70 && average >= 60)
+            {
+                lblClassTitle.Text = "C";
+            }
+            else if (average < 60 && average >= 50)
+            {
+                lblClassTitle.Text = "D";
+            }
+            else
+            {
+                lblClassTitle.Text = "F";
+            }
+
+            lblCrTitle1.Text = classTitles[0];
+            lblCrTitle2.Text = classTitles[1];
+            lblCrTitle3.Text = classTitles[2];
+            lblCrTitle4.Text = classTitles[3];
+            lblCrTitle5.Text = classTitles[4];
+            lblCrTitle6.Text = classTitles[5];
+        }
+
+       
+    }
+}
