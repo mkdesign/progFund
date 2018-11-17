23527b9b65e1980a33ea7ed68f123968b34a0635
mkdesign <mehrad.kavian@gmail.com>
2018-11-17 05:48:22 +0000

    LAb 5 - Added Could not add it's own branch so I had to copy it. Own branch had few more commits.Still Working on that.

---
+++ lab5/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblAlert = new System.Windows.Forms.Label();
+            this.btnLoop = new System.Windows.Forms.Button();
+            this.textPass = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblAlert
+            // 
+            this.lblAlert.AutoSize = true;
+            this.lblAlert.Location = new System.Drawing.Point(246, 60);
+            this.lblAlert.Name = "lblAlert";
+            this.lblAlert.Size = new System.Drawing.Size(148, 13);
+            this.lblAlert.TabIndex = 0;
+            this.lblAlert.Text = "Click the button to start a loop";
+            // 
+            // btnLoop
+            // 
+            this.btnLoop.Location = new System.Drawing.Point(249, 96);
+            this.btnLoop.Name = "btnLoop";
+            this.btnLoop.Size = new System.Drawing.Size(75, 23);
+            this.btnLoop.TabIndex = 1;
+            this.btnLoop.Text = "button1";
+            this.btnLoop.UseVisualStyleBackColor = true;
+            this.btnLoop.Click += new System.EventHandler(this.btnLoop_Click);
+            // 
+            // textPass
+            // 
+            this.textPass.Location = new System.Drawing.Point(364, 98);
+            this.textPass.Name = "textPass";
+            this.textPass.Size = new System.Drawing.Size(100, 20);
+            this.textPass.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textPass);
+            this.Controls.Add(this.btnLoop);
+            this.Controls.Add(this.lblAlert);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblAlert;
+        private System.Windows.Forms.Button btnLoop;
+        private System.Windows.Forms.TextBox textPass;
+    }
+}
+
---
+++ lab5/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            textPass.PasswordChar = '*';
+        }
+
+        private void btnLoop_Click(object sender, EventArgs e)
+        {
+
+            string passVal = "password";
+            string userPass = textPass.Text;
+            
+            if(userPass!= passVal)
+            {
+                lblAlert.Text = "Wrong Password.Please Try again";
+                textPass.Text = String.Empty;
+
+            }
+            else
+            {
+                lblAlert.Text = "Welcome!";
+                btnLoop.Visible = false;
+                textPass.Visible = false;
+            }
+
+           
+
+
+            
+        }
+    }
+}
